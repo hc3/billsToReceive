df6748a2f7d9f71127233493f395228980cbcdbb
hc3 <eliel.floyd@bol.com.br>
2016-04-09 15:26:27 -0300

    adicionado um sql com as cidades do Brasil q não sera usado nesse projeto

---
+++ billToReceive/database/migrations/2016_04_09_162805_create_clientes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('cpf');
+            $table->integer('endereco_id')->unsigned()->nullable();
+            $table->foreign('endereco_id')->references('id')->on('enderecos')->onDelete('cascade');
+
+            $table->integer('cidade_id')->unsigned()->nullable();
+            $table->foreign('cidade_id')->references('id')->on('cidades');
+
+            $table->integer('contato_id')->unsigned()->nullable();
+            $table->foreign('contato_id')->references('id')->on('contatos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clientes');
+    }
+}
--- billToReceive/database/seeds/DatabaseSeeder.php
+++ billToReceive/database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
+
 {
     /**
      * Run the database seeds.
@@ -12,5 +13,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call('CidadesTableACSeeder');
+        $this->call('CidadesTableALSeeder');
+        $this->call('CidadesTableAMSeeder');
+        $this->call('CidadesTableAPSeeder');
+        $this->call('CidadesTableBASeeder');
+        $this->call('CidadesTableCESeeder');
+        $this->call('CidadesTableDFSeeder');
+        $this->call('CidadesTableESSeeder');
+        $this->call('CidadesTableGOSeeder');
+        $this->call('CidadesTableMASeeder');
+        $this->call('CidadesTableMGSeeder');
+        $this->call('CidadesTableMSSeeder');
+        $this->call('CidadesTableMTSeeder');
+        $this->call('CidadesTablePASeeder');
+        $this->call('CidadesTablePBSeeder');
+        $this->call('CidadesTablePISeeder');
+        $this->call('CidadesTablePRSeeder');
+        $this->call('CidadesTableRJSeeder');
+        $this->call('CidadesTableRNSeeder');
+        $this->call('CidadesTableROSeeder');
+        $this->call('CidadesTableRRSeeder');
+        $this->call('CidadesTableRSSeeder');
+        $this->call('CidadesTableSCSeeder');
+        $this->call('CidadesTableSESeeder');
+        $this->call('CidadesTableSPSeeder');
+        $this->call('CidadesTableTOSeeder');
+        $this->call('EstadosTableSeeder');
     }
 }
